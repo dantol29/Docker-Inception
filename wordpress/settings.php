66496c7e9598d5028898f26410f0a487cc4f2172
dantol29 <dantol29@gmail.com>
2024-06-02 20:43:24 +0100

    WIP: trying to connect mariadb

--- wordpress/settings.php
+++ wordpress/settings.php
@@ -5,17 +5,18 @@ define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
 define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
 define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
 
-// Database charset
-define( 'DB_CHARSET', 'utf8' );
+// // Database charset
+// define( 'DB_CHARSET', 'utf8' );
 
-// Database collation
-define( 'DB_COLLATE', '' );
+// // Database collation
+// define( 'DB_COLLATE', '' );
 
 // WordPress database table prefix
 $table_prefix = 'wp_';
 
 // Enable debug mode for troubleshooting
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 
 // Absolute path to the WordPress directory
 if (! defined('ABSPATH') )
