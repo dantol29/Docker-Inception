30a6362079d16366540c94a55e40cba20bfba80d
dantol29 <dantol29@gmail.com>
2024-06-02 17:48:21 +0100

    feat(wordpress): add settings file

--- wordpress/settings.php
+++ wordpress/settings.php
@@ -11,16 +11,6 @@ define( 'DB_CHARSET', 'utf8' );
 // Database collation
 define( 'DB_COLLATE', '' );
 
-// Authentication unique keys and salts
-define( 'AUTH_KEY',         getenv('WORDPRESS_AUTH_KEY') );
-define( 'SECURE_AUTH_KEY',  getenv('WORDPRESS_SECURE_AUTH_KEY') );
-define( 'LOGGED_IN_KEY',    getenv('WORDPRESS_LOGGED_IN_KEY') );
-define( 'NONCE_KEY',        getenv('WORDPRESS_NONCE_KEY') );
-define( 'AUTH_SALT',        getenv('WORDPRESS_AUTH_SALT') );
-define( 'SECURE_AUTH_SALT', getenv('WORDPRESS_SECURE_AUTH_SALT') );
-define( 'LOGGED_IN_SALT',   getenv('WORDPRESS_LOGGED_IN_SALT') );
-define( 'NONCE_SALT',       getenv('WORDPRESS_NONCE_SALT') );
-
 // WordPress database table prefix
 $table_prefix = 'wp_';
 
@@ -28,8 +18,7 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 // Absolute path to the WordPress directory
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(FILE) . '/');
+define('ABSPATH', dirname(FILE) . '/');
 
 // Sets up WordPress vars and included files
 require_once( ABSPATH . 'wp-settings.php' );
