f0c4709785f07e730161b4028833c00c094b2501
dantol29 <dantol29@gmail.com>
2024-06-03 21:54:48 +0100

    feat(wordpress): add guest user

--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -14,7 +14,12 @@ fi
 if ! wp core is-installed --allow-root; then
     echo "creating website"
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-    wp user create guest guest@gmail.com --role=editor --user_pass=dantol2004 --allow-root
+fi
+
+if wp user list --allow-root | grep -q $WP_GUEST_USER; then
+    echo "User exists"
+else
+    wp user create $WP_GUEST_USER $WP_GUEST_EMAIL --role=editor --user_pass=$WP_GUEST_PASSWORD --allow-root
 fi
 
 # Execute the CMD
